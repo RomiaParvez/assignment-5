c7a206d13ddac071da141daa5ce695aa576f8187
RomiaParvez <romiaparvez@gmail.com>
2025-03-31 17:56:19 +0530

    Create slicing.py

---
+++ slicing.py
@@ -0,0 +1,12 @@
+# Creating a list of numbers from 1 to 10
+numbers = list(range(1, 11))
+
+# Extracting the first five elements
+first_five = numbers[:5]
+
+# Reversing these extracted elements
+reversed_five = first_five[::-1]
+
+# Printing both lists
+print("Extracted list:", first_five)
+print("Reversed list:", reversed_five)
